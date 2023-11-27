40e1aa6d1ae1c93d5a0341af58afb286f4f8ef3e
ashledombos <rj@hodo.fr>
2023-11-27 18:09:05 +0100

    Update _common.sh

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -15,6 +15,8 @@ virtual_entry="$list_name@$domain    $app@localhost.mlmmj"
 
 CRON_DIR="/etc/cron.d/mlmmj"
 cron_file="CRON_DIR/$app.cron"
+MLMMJRECEIVE="/usr/local/bin/mlmmj-receive"
+MLMMJMAINTD="/usr/local/bin/mlmmj-maintd"
 
 installed_version=""
 REQUIRED_VERSION=$(cat ../mlmmj_version)
