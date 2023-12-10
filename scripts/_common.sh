c986c2786efd6760e7a1afea74b2198875c43a20
ashledombos <rj@hodo.fr>
2023-12-10 20:07:11 +0100

    Update _common.sh

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -37,33 +37,25 @@ install_update_mlmmj() {
 
     if command -v mlmmj-list >/dev/null 2>&1; then
         installed_version=$(mlmmj-list -V | cut -d ' ' -f3)
-        ynh_script_progression --message="mlmmj is already installed with version: $installed_version"
 
         if [ "$required_version" != "$installed_version" ]; then
-            ynh_script_progression --message="Updating mlmmj to the required version $required_version;"
+            ynh_script_progression --message="Updating mlmmj from $installed_version to $required_version;"
+        else 
+            ynh_script_progression --message="mlmmj is already installed with version: $installed_version"
         fi
     else
         ynh_script_progression --message="mlmmj is not installed, proceeding with installation;"
     fi
 
     if [ "$required_version" != "$installed_version" ]; then
-        ynh_script_progression --message="Installing necessary packages"
+        ynh_script_progression --message="Building mlmmj software"
         ynh_install_app_dependencies autoconf make gcc pkg-config libatf-dev
-        ynh_script_progression --message="Downloading and installing mlmmj release"
         ynh_setup_source --dest_dir="mlmmj-latest" || ynh_die "Failed to download mlmmj"
         pushd mlmmj-latest
         autoreconf -i && ./configure && make && sudo make install || ynh_die "Failed to install mlmmj binaries"
         popd
     fi
 
-    ynh_script_progression --message="Potfix setup"
-    #! ls $HOOK_DIR/*-mlmmj 1> /dev/null 2>&1 &&
-    #cp ../hooks/conf_regen /usr/share/yunohost/hooks/conf_regen/98-mlmmj
-    #yunohost tools regen-conf
-    #rm -f /usr/share/yunohost/hooks/conf_regen/98-mlmmj
-    #[ -f $TEMP_HOOK_PATH ] && rm $TEMP_HOOK_PATH
-
-    ynh_script_progression --message="Setting ACLs"
     ynh_system_user_create --username=mlmmj_pfx
 
     
