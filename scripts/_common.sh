efde8abcc84747589636539f38ab84e4128ab2a6
ashledombos <rj@hodo.fr>
2024-01-27 23:57:44 +0100

    Update _common.sh

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -17,8 +17,10 @@ subscribers_dir="$data_dir/subscribers.d"
 ynh_dir="$data_dir/ynh"
 
 # List email address variables
-local_part="${list_email%@*}"
-domain_part="${list_email#*@}"
+# local_part="${list_email%@*}"
+# domain_part="${list_email#*@}"
+local_part="toto"
+domain_part="rototo"
 transport_entry="$app@localhost.mlmmj       mlmmj:$app"
 virtual_entry="$list_email    $app@localhost.mlmmj"
 
@@ -37,31 +39,31 @@ lang_dir="$mlmmj_share/text.skel/$language"
 #=================================================
 # PERSONAL HELPERS
 #=================================================
-install_update_mlmmj() {
-        ynh_print_info --message="Building mlmmj software"
-        ynh_install_app_dependencies autoconf make gcc pkg-config libatf-dev
+# install_update_mlmmj() {
+#         ynh_print_info --message="Building mlmmj software"
+#         ynh_install_app_dependencies autoconf make gcc pkg-config libatf-dev
 
-        [ ! -d "$src_dir" ] && mkdir -p "$src_dir"
-        [ ! -d "$app_dir" ] && mkdir -p "$app_dir"
+#         [ ! -d "$src_dir" ] && mkdir -p "$src_dir"
+#         [ ! -d "$app_dir" ] && mkdir -p "$app_dir"
         
-        ynh_setup_source  --full_replace --dest_dir="$src_dir" || ynh_die "Failed to download mlmmj"
+#         ynh_setup_source  --full_replace --dest_dir="$src_dir" || ynh_die "Failed to download mlmmj"
 
-        chown $app:$app "$src_dir" "$app_dir" -R
-        pushd "$src_dir"
+#         chown $app:$app "$src_dir" "$app_dir" -R
+#         pushd "$src_dir"
 
-        ynh_exec_and_print_stderr_only_if_error sudo -u $app autoreconf -i
-        ynh_exec_and_print_stderr_only_if_error sudo -u $app ./configure  --prefix="$app_dir"
-        ynh_exec_and_print_stderr_only_if_error sudo -u $app make
-        ynh_exec_and_print_stderr_only_if_error sudo -u $app make install
+#         ynh_exec_and_print_stderr_only_if_error sudo -u $app autoreconf -i
+#         ynh_exec_and_print_stderr_only_if_error sudo -u $app ./configure  --prefix="$app_dir"
+#         ynh_exec_and_print_stderr_only_if_error sudo -u $app make
+#         ynh_exec_and_print_stderr_only_if_error sudo -u $app make install
     
-        if [ $? -ne 0 ]; then
-            ynh_die "Failed to install mlmmj binaries"
-        fi
+#         if [ $? -ne 0 ]; then
+#             ynh_die "Failed to install mlmmj binaries"
+#         fi
     
-        popd
-        ynh_add_config --template="regenconf_postfix" --destination="$regen_conf_file"
-        yunohost tools regen-conf postfix
-}
+#         popd
+#         ynh_add_config --template="regenconf_postfix" --destination="$regen_conf_file"
+#         yunohost tools regen-conf postfix
+# }
 #=================================================
 # EXPERIMENTAL HELPERS
 #=================================================
